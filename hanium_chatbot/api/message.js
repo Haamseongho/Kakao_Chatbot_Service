3ef1e3b61e030d630e060192967b5c0caf0dfe37
root <root@ip-172-31-11-196.ap-northeast-2.compute.internal>
2017-08-29 04:39:45 +0000

    messaging error correct

--- hanium_chatbot/api/message.js
+++ hanium_chatbot/api/message.js
@@ -7,19 +7,10 @@ message.buttons = ['어디가 아프신가요?.? ', '현재 위치 상 가까운
 
 message.buttonsType = () => {
     return {
-        message: {
-            "message": {
-                "text": "안녕하세요 사용자에게 맞는 병원을 소개해주는 맞춤형 테라피 서비스 플랫폼 '헬프미닥터' 입니다. " +
-                "단 세 가지 질문으로 사용자에게 맞는 병원을 소개해드립니다." + " 처음 응답형 버튼을 시작으로 질문에 답변해 주시기 바랍니다. "
-                + " 감사합니다! "
-            },
-            "keyboard": {
-                "type": "buttons",
+                type: "buttons",
                 buttons: message.buttons
             }
-        }
-    }
-};
+      };
 
 message.baseType = (text) => {
     return {
