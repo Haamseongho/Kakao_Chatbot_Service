10f35023f7250f9b66a45072f55a1d88fbe2eb5a
haamseongho <haams704@gmail.com>
2017-10-05 17:40:35 +0900

    '구'로 분류하기

---
+++ hanium_chatbot/api/location/setLocationService.js
@@ -0,0 +1,13 @@
+/**
+ * Created by haams on 2017-10-05.
+ */
+var express = require("expres");
+var router = express.Router();
+
+module.exports = function (router,index) {
+    switch (index){
+        case 4:
+
+            break;
+    }
+}
